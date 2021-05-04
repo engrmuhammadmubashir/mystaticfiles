e0df3d7a6b66365d0dbffcc178b6dc3a4d8b6ff4
engrmuhammadmubashir <82230003+engrmuhammadmubashir@users.noreply.github.com>
2021-05-04 18:16:51 +0500

    Static Files For Website
    Bootstrap, MDBootstrap, JQuery

---
+++ mystaticfiles/js/app.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+        // animateOut: 'slideOutDown',
+        // animateIn: 'flipInX',
+        // items: 1,
+        items: 4,
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        autoplayTimeout: 1000,
+        autoplayHoverPause: true
+    });
+    $('.play').on('click', function () {
+        owl.trigger('play.owl.autoplay', [1000])
+    })
+    $('.stop').on('click', function () {
+        owl.trigger('stop.owl.autoplay')
+    })
+
+});
+
+// Animations init
+new WOW().init();
